a00bd03a5e5123100fe26fdbd1a02e40d5ba55ec
Klap-in <klapinklapin@gmail.com>
2015-07-30 22:54:53 +0200

    translation update

---
+++ lang/eo/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Klap-in <klapinklapin@gmail.com>
+ */
+$lang['imgshort']              = 'Test';
+$lang['tabshort']              = 'test';
+$lang['imgfull']               = 'test';
+$lang['tabfull']               = 'tesstt';
