d66cfbdce5499bb5629e584a4294804867a1028e
Klap-in <klapinklapin@gmail.com>
2015-07-30 23:40:24 +0200

    translation update

---
+++ lang/sq/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Klap-in <klapinklapin@gmail.com>
+ */
+$lang['imgshort']              = 'aa';
+$lang['tabshort']              = 'ff';
+$lang['imgfull']               = 'dd';
+$lang['tabfull']               = 'ss';
