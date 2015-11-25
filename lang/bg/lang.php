e356d86f43451d53b8538b0a383870702e0c9328
Neli Dimitrova <neli.dimitrova.office@gmail.com>
2015-11-25 12:25:08 +0100

    translation update

---
+++ lang/bg/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Neli Dimitrova <neli.dimitrova.office@gmail.com>
+ */
+$lang['imgshort']              = 'Фиг.';
+$lang['tabshort']              = 'Табл.';
+$lang['imgfull']               = 'фигура';
+$lang['tabfull']               = 'таблица';
