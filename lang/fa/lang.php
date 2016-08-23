ce98c19e3b7c9dee53e598328e7eb4275c4f9a51
Sam01 <m.sajad079@gmail.com>
2016-08-23 07:25:38 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['imgshort']              = 'شکل.';
+$lang['tabshort']              = 'برگه.';
+$lang['imgfull']               = 'شکل';
+$lang['tabfull']               = 'جدول';
