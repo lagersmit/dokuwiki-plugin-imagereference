416357b7ca17db1b1251e3a392ff7ad156b31478
g <a@b.nl>
2016-09-27 00:18:02 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,9 +1,10 @@
 <?php
-/**
- * french language file
- */
 
-$lang['imgshort'] = 'Fig.';
-$lang['tabshort'] = 'Tab.';
-$lang['imgfull']  = 'figure';
-$lang['tabfull']  = 'table';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['imgshort']              = 'Fig.';
+$lang['tabshort']              = 'Tab.';
+$lang['imgfull']               = 'figure';
+$lang['tabfull']               = 'table';
