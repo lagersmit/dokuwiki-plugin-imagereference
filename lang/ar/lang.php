3e4bb2618ed363fb0ebaa6d7b23c501932d19fe4
Klap-in <klapinklapin@gmail.com>
2016-09-27 00:17:51 +0200

    translation update

---
+++ lang/ar/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Klap-in <klapinklapin@gmail.com>
+ */
+$lang['imgshort']              = 'a';
+$lang['tabshort']              = 'af';
+$lang['imgfull']               = 'f';
+$lang['tabfull']               = 's';
