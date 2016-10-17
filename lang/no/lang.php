aba831d18e2f7410abeed2ef9b5538d62caacfb6
Arne Hanssen <arne.hanssen@getmail.no>
2016-10-17 10:40:33 +0200

    translation update

---
+++ lang/no/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Arne Hanssen <arne.hanssen@getmail.no>
+ */
+$lang['imgshort']              = 'Fig.';
+$lang['tabshort']              = 'Tab.';
+$lang['imgfull']               = 'figur';
+$lang['tabfull']               = 'tabell';
